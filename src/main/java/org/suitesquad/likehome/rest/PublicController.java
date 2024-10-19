e804a93d24e095050dd3af1bc2e80d04c2bafde0
sZeru <kolby.salmond@yahoo.com>
2024-10-19 16:45:06 -0700

    Implemented basic DB interface and connection.
    Access the db via UserService.
    The db connection is local currently. Will port to cloud later.
    Not all user fields are implemented yet.

--- src/main/java/org/suitesquad/likehome/rest/PublicController.java
+++ src/main/java/org/suitesquad/likehome/rest/PublicController.java
@@ -1,8 +1,14 @@
 package org.suitesquad.likehome.rest;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.suitesquad.likehome.service.UserService;
+import org.suitesquad.likehome.model.User;
+
+import javax.swing.*;
+import java.util.List;
 
 /**
  * This class handles all requests not requiring authentication.
@@ -11,9 +17,21 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping
 public class PublicController {
 
+    @Autowired
+    private UserService userService;
+
     @GetMapping("/ping")
     public String ping() {
         return "pong";
     }
 
+    // Example of how to access database. Create a userService and perform the appropriate operation.
+    @GetMapping("/db")
+    public String db() {
+        List<User> userList = userService.fetchAllUserData();
+        if (userList != null && !userList.isEmpty()) {
+            return userList.getFirst().getEmail();  // returns the email of the first user in our database
+        }
+        return "Failure, no users found";
+    }
 }
