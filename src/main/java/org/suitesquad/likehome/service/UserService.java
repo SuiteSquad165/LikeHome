78ce24e72bec4daf35bf8fca06580aea57e227f8
sZeru <kolby.salmond@yahoo.com>
2024-10-20 18:51:42 -0700

    Added Hotel and Reservation collections.
    Known bug in getting Reservation fields.

--- src/main/java/org/suitesquad/likehome/service/UserService.java
+++ src/main/java/org/suitesquad/likehome/service/UserService.java
@@ -24,10 +24,6 @@ public class UserService {
         return userRepo.findAll();
     }
 
-    //public String fetchUserDataByName(String name) {
-    //    return userRepo.findByNameIgnoreCase(name);
-    //}
-
     public void deleteUserData(User userDetails) {
         userRepo.delete(userDetails);
     }
