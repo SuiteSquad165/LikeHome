fd8b7b5a80fc6b32dcb6801957b10f0c816fd1bf
airsquared <36649395+airsquared@users.noreply.github.com>
2024-11-30 18:40:54 -0800

    Cleanup code

--- src/main/java/org/suitesquad/likehome/repository/ReviewRepository.java
+++ src/main/java/org/suitesquad/likehome/repository/ReviewRepository.java
@@ -9,6 +9,4 @@ import java.util.List;
 @Repository
 public interface ReviewRepository extends MongoRepository<Review, String> {
     List<Review> findByHotelId(String hotelId);
-
-    void updateReviewById(String id, Review reviewDetails);
 }
